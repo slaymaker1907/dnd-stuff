936a89b01623a013b5a241eead15fd804fd5ae20
Dyllon Gagnier <dyllongagnier@gmail.com>
2016-07-10 16:34:17 -0600

    Made calculator use mustache templates

---
+++ ability_score/template.py
@@ -0,0 +1,13 @@
+import pystache
+
+def make_template():
+    with open('ability_score.mustache') as f:
+        template = f.read()
+    attributes = ['Strength', 'Dexterity', 'Constitution', 'Intellect', 'Wisdom', 'Charisma']
+    lst = [{'name':name} for name in attributes]
+    lst[-1]['last'] = True
+    with open('ability_score.html', 'w') as f:
+        f.write(pystache.render(template, {'attributes':lst, 'jquery':'jquery-2.2.4.min.js'}))
+
+if __name__ == '__main__':
+    make_template()
