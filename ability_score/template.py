7da4294fadcf52759a033d68f5b429d571870ff7
Dyllon Gagnier <dyllongagnier@gmail.com>
2016-07-10 17:40:51 -0600

    Made use bootstrap.

--- ability_score/template.py
+++ ability_score/template.py
@@ -7,7 +7,7 @@ def make_template():
     lst = [{'name':name} for name in attributes]
     lst[-1]['last'] = True
     with open('ability_score.html', 'w') as f:
-        f.write(pystache.render(template, {'attributes':lst, 'jquery':'jquery-2.2.4.min.js'}))
+        f.write(pystache.render(template, {'attributes':lst, 'jquery':'jquery-2.2.4.min.js', 'bootstrap':'bootstrap-3.3.6/dist/css/bootstrap.min.css'}))
 
 if __name__ == '__main__':
     make_template()
